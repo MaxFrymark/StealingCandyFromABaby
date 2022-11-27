899d5199d5577088e916ac249189e91e24ad9ffe
MaxFrymark <94791525+MaxFrymark@users.noreply.github.com>
2022-11-27 12:42:04 -0500

    Build 2

--- Assets/ExitArrow.cs
+++ Assets/ExitArrow.cs
@@ -6,9 +6,11 @@ public class ExitArrow : MonoBehaviour
 {
     [SerializeField] SceneLoader loader;
     [SerializeField] GameObject goodJob;
+    [SerializeField] GameObject messages;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        messages.SetActive(false);
         goodJob.SetActive(true);
         collision.GetComponent<Player>().SetHasAlreadyWon();
         loader.LoadNextLevel();
--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -6,9 +6,11 @@ public class GameOverScreen : MonoBehaviour
 {
     [SerializeField] InputHandler inputHandler;
     [SerializeField] MenuInputHandler menuInput;
+    [SerializeField] GameObject messages;
 
     private void OnEnable()
     {
+        messages.SetActive(false);
         inputHandler.gameObject.SetActive(false);
         menuInput.gameObject.SetActive(true);
     }
